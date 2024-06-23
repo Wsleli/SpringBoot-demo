2c8402bd21d126738a1116c1a36d43eaef4a3277
wsleli <aiyingggg@gmail.com>
2024-06-24 00:31:39 +0800

    多环境开发兼容问题（Maven与Spring Boot）

--- src/main/java/com/wsleli/controller/BookController.java
+++ src/main/java/com/wsleli/controller/BookController.java
@@ -1,15 +1,9 @@
 package com.wsleli.controller;
 
-import com.wsleli.domain.Enterprise;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
 
 /**
  * @Description: 功能描述
@@ -20,35 +14,10 @@ import java.util.Arrays;
 @RestController
 @RequestMapping("/books")
 public class BookController {
-    @Value("${lesson}")
-    private String lesson;
-    @Value("${server.port}")
-    private Integer port;
-    @Value("${enterprise.subject[0]}")
-    private String subject_00;
-
-    @Autowired
-    private Environment env;
-
-    @Autowired
-    private Enterprise enterprise;
 
     @GetMapping("/{id}")
-    public String getById(@PathVariable Integer id){
-        System.out.println(lesson);
-        System.out.println(port);
-        System.out.println(subject_00);
-        System.out.println("---------------------------------------");
-        System.out.println(env.getProperty("lesson"));
-        System.out.println(env.getProperty("enterprise.name"));
-        System.out.println(env.getProperty("enterprise.subject[0]"));
-        System.out.println("---------------------------------------");
-        System.out.println(enterprise.getName());
-        System.out.println(enterprise.getAge());
-        System.out.println(Arrays.toString(enterprise.getSubject()));
-        System.out.println(enterprise.getTel());
-        System.out.println(enterprise.getSubject()[0]);
-        System.out.println(enterprise);
+    public String getById(@PathVariable Integer id) {
+        System.out.println("==>" + id);
         return "hello , spring boot!";
     }
 }
